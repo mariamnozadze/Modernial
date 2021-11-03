a675984ba53afd19c81277020234cb235c8e5a9d
mariamnozadze <mariamnozadze97@yahoo.com>
2021-11-03 15:43:12 +0400

    fix gsap

---
+++ index.js
@@ -0,0 +1,7 @@
+    gsap.from(".logo", { opacity: 0, duration: 1, delay: 0.5, y: -100 });
+    gsap.from("nav", { opacity: 0, duration: 1, delay: 0.5, x: 20 });
+    gsap.from(".image", { opacity: 0, duration: 1, delay: 1, x: -100 });
+    gsap.from(".content", { opacity: 0, duration: 1, delay: 1, x: 100 });
+    gsap.from(".call-to-action", { opacity: 0, duration: 1, delay: 0.5, y: 100 });
+    gsap.from(".message-1", { opacity: 0, duration: 1, delay: 0.5, x: -100 });
+    gsap.from(".message-2", { opacity: 0, duration: 1, delay: 0.5, x: 100 });
